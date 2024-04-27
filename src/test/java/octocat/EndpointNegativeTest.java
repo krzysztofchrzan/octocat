55df1d18f9ae3453a65bcf453134ad93031188e4
admin <krzysiek.chrzan@gmail.com>
2024-04-27 18:40:19 +0200

    Get endpoint test created

---
+++ src/test/java/octocat/EndpointNegativeTest.java
@@ -0,0 +1,60 @@
+package octocat;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class EndpointNegativeTest {
+    private final String BASE_URL = "https://api.github.com/users";
+    private Response response = null;
+    RequestSpecification requestSpecification = null;
+
+    @BeforeClass
+    public void setUp() {
+        requestSpecification = RestAssured.given();
+        requestSpecification.header("Content-Type", "application/json");
+    }
+
+    @Test(priority = 1)
+    public void testPutMethod() {
+        response = requestSpecification.get(BASE_URL + "/octocat");
+        String responseBody = response.getBody().asString();
+        JSONObject requestJSON;
+        // changing value in body request
+        try {
+            requestJSON = new JSONObject(responseBody);
+            requestJSON.put("location", "Warszawa");
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+        requestSpecification.body(requestJSON.toString());
+        response = requestSpecification.put(BASE_URL);
+        Assert.assertEquals(response.statusCode(), 404);
+    }
+
+    @Test(priority = 2)
+    public void testPostMethod() {
+        response = requestSpecification.get(BASE_URL + "/octocat");
+        String responseBody = response.getBody().asString();
+        JSONObject requestJSON;
+        try {
+            requestJSON = new JSONObject(responseBody);
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+        requestSpecification.body(requestJSON.toString());
+        response = requestSpecification.post(BASE_URL);
+        Assert.assertEquals(response.statusCode(), 404);
+    }
+
+    @Test(priority = 3)
+    public void testDeleteStatusCode() {
+        response = requestSpecification.delete(BASE_URL + "/octocat");
+        Assert.assertEquals(response.statusCode(), 404);
+    }
+}
